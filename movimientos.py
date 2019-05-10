b3837fda278b79d1b1fdbdbd182badbfdbf47215
DSAZ1324 <darteag94278@universidadean.edu.co>
2019-05-09 19:45:57 -0500

    Correciones

--- movimientos.py
+++ movimientos.py
@@ -44,6 +44,31 @@ def obtener_nombre_pieza(simbolo):
     else:
         return 'No es una pieza'
 
+def mover_torre1(tablero, x_inicial, y_inicial, x_final, y_final):
+    """
+    (list of list, int, int, int, int) -> list of list
+    :param tablero: list of list que representa el tablero
+    :param x_inicial: int que representa la posicion inicial en X
+    :param y_inicial: int que representa la posicion inicial en Y
+    :param x_final: int que representa la posicion final en X
+    :param y_final: int que representa la posicion final en Y
+    :return: list of list que representa un tablero final
+    """
+    tab = tablero.copy()
+    if (x_inicial == x_final or y_inicial == y_final) and tab[x_inicial][y_inicial].lower() == 't':
+            if x_inicial != x_final:
+                for x in range(x_inicial +1, x_final):
+                    if tab[x][y_inicial] != ' ':
+                        raise ValueError('El camino no es valido')
+            tab[x_final][y_inicial] = 't'
+            tab[x_inicial][y_inicial] = ' '
+            if y_inicial != y_final:
+                for y in range(y_inicial +1, y_final):
+                    if tab[x_inicial][y] != ' ':
+                        raise ValueError('El camino no es valido')
+            tab[x_inicial][y_final] = 't'
+            tab[x_inicial][y_inicial] = ' '
+    return tab
 
 def mover_torre(tablero, x_inicial, y_inicial, x_final, y_final):
     """
@@ -98,8 +123,8 @@ def mover_alfil(tablero, x_inicial, y_inicial, x_final, y_final):
     :return: list of list que representa un tablero final
     """
     tab = tablero.copy()
-    if (x_inicial - y_inicial == x_final - y_final) and (x_inicial + y_inicial == x_final + y_final) and tab[x_inicial][y_final]. lower() == 'a':
-        if x_inicial - x_final:
+    if ((x_inicial - y_inicial == x_final - y_final) or (x_inicial + y_inicial == x_final + y_final)) and tab[x_inicial][y_final].lower() == 'a':
+        if x_inicial - y_final:
             for x in range(x_inicial +1, y_final):
                 if tab[x][y_inicial] != ' ':
                     raise ValueError('El camino no es valido')
